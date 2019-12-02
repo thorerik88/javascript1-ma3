62379ad7f2ed44d0b2fd0a19c3f3a3a9a117b156
Thor-Erik <thorerik88@hotmail.com>
2019-12-02 12:09:15 +0100

    initial commit

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,62 @@
 //Use RegEx to validate form
+
+// set global variables
+const names = document.querySelectorAll("input[type='name']");
+const formSubmit = document.querySelector("input[type='submit']");
+let validateRegex = false;
+let validateNames = false;
+let i = "";
+let message = [];
+
+// Output error messages
+function errorOutput(messageArray) {
+  for (i = 0; i < messageArray.length; i++) {
+    console.log(messageArray[i] + "\n");
+  }
+}
+
+// Validate that name inputs are not empty
+function valNames(){
+  for (i = 0; i < names.length; i++) {
+    if (!names[i].value) {
+      validateNames = false;
+      message.push("You must type your first AND last name");
+      break
+    } else {
+      validateNames = true;
+    }
+  }
+}
+
+// Validate phone and email
+function valRegex() {
+  let phoneInput = document.querySelector("input[type='tel']").value;
+  let emailInput = document.querySelector("input[type='email']").value;
+  const phoneRegex = /[\d]/;
+  const emailRegex = /^.+@.+\..+$/;
+  let phoneResult = phoneRegex.test(phoneInput.trim());
+  let emailResult = emailRegex.test(emailInput.trim());
+  if (phoneResult & emailResult) {
+    validateRegex = true;
+  } else {
+    validateRegex = false;
+    if (!emailResult){
+      message.push("Incorrect email address format");
+    }
+    if (!phoneResult) {
+      message.push("Incorrect telephone number format");
+    }
+  }
+}
+
+// Check if form is validated correctly
+formSubmit.addEventListener("click", function(){
+  valRegex(); // call to check regex function
+  valNames(); // call to check names function
+  if (validateRegex && validateNames) {
+    console.log("The form has been sent")
+  } else {
+    errorOutput(message.reverse()); // output message to error function, reversed to more logical order
+    message = [];
+  }
+});
--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,29 @@
 //Console.log out elements in JSON file
+let json = {
+	"video": [{
+		"id": 12312412312,
+		"name": "Ecuaciones Diferenciales",
+		"url": "/video/math/edo/12312412312",
+		"author": {
+			"data": [{
+				"name_author": "Alejandro Morales",
+				"url": "/author/alejandro-morales",
+				"type": "master"
+			}]
+		}
+	}]
+}
+
+for (let key in json) {
+  let value = json[key][0];
+  console.log(value.id);
+  console.log(value.name);
+  console.log(value.url);
+  console.log(value.author);
+  let data = value.author.data;
+  for (let key in data) {
+    console.log(data[key].name_author);
+    console.log(data[key].url);
+    console.log(data[key].type);
+  }
+}
